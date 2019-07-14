97e7f76689ac05995964e9aec9d04892ed4baef6
Alex <block011@cougars.csusm.edu>
2019-07-13 19:48:26 -0700

    split config file into open and private

---
+++ tracker/config.py
@@ -0,0 +1,17 @@
+
+#Current available regions
+ALL_REGIONS = {
+    'BR':'BR1',
+    'EUN':'EUN1',
+    'EUW':'EUW1',
+    'JP':'JP1',
+    'KR':'KR',
+    'LA1':'LA1',
+    'LA2':'LA2',
+    'NA':'NA1',
+    'OC':'OC1',
+    'TR':'TR1',
+    'RU':'RU',
+    'PBE':'PBE1'}
+
+MATCH_FINDER_FILE_NAME = 'MatchFinder.py'
