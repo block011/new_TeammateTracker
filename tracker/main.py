8342653cd352be5304864f08c534b21d2c76cad7
Alex <block011@cougars.csusm.edu>
2019-07-14 15:52:40 -0700

    added constants and moved file path into main

--- tracker/main.py
+++ tracker/main.py
@@ -3,9 +3,10 @@ import time
 import os
 from subprocess import Popen
 
-from config import *
-from SensitiveConfig import *
+from config import MATCH_FINDER_FILE_NAME,  MATCH_FINDER_SCHEDULE_TIME
 
+#Used to grab file path of main.py
+CURRENT_FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 
 def runMatchFinder():       
     Popen('python {}/{}'.format(CURRENT_FILE_PATH,MATCH_FINDER_FILE_NAME))
@@ -14,12 +15,12 @@ def runMatchFinder():
 #TODO: need to add logging mechanism
 def main():
     
-    schedule.every(10).minutes.do(runMatchFinder)
+    schedule.every(MATCH_FINDER_SCHEDULE_TIME).minutes.do(runMatchFinder)
 
     while(True):
         try:
             schedule.run_pending()
-            time.sleep(600) #Sleeping for 10 minutes 
+            time.sleep(60) #Sleeping for 1 minute(s)
         except KeyboardInterrupt:
             print("ManualBreak")
             break
