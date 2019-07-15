3100af3f60897dee05754ef4832fc667232b9222
Alex <block011@cougars.csusm.edu>
2019-07-14 22:04:30 -0700

    progress script

--- tracker/MatchFinder.py
+++ tracker/MatchFinder.py
@@ -6,8 +6,23 @@ from config import *
 import requests
 
 
+def fetchActiveUsers():
+    ''' fetches all active users'''
+    #accessing active database
+    dbConnection = ActiveDatabase().getConn()
+    userConnection= dbConnection.collection(u'users')
+    try:
+        userList = userConnection.stream()
+        success = True
+    except Exception as e:
+        print(e)
+        userList = None
+        success = False
+    
+    return userList,success
+
 def fetchMatchEntries(user):
-    ''' fetched n number of matches from RiotGames api
+    ''' fetches n number of matches from RiotGames api
     Args: dictionary
         region: "NA1"
         accountId: "6UoWroRfGgxqnI0Oj-KahipCC2TcQWiKkAar-BWyYGoxAw"
@@ -15,32 +30,70 @@ def fetchMatchEntries(user):
     Returns:
         TODO:   
     '''
+    try:
+        url = RIOT_FETCH_MATCH_URL.format(user["region"], user["accountId"], RIOT_API_KEY, MAX_MATCH_FETCH)
+        response = requests.get(url).json()
+        matchList = response['matches']
+        success = True
+    except Exception as e:
+        print(e)
+        matchList = None
+        success = False
+    
+    return matchList, success
 
-    url = RIOT_FETCH_MATCH_URL.format(user["region"], user["accountId"], RIOT_API_KEY, MAX_MATCH_FETCH)
-    print(url)
-    response = requests.get(url).json()
-    for match in response['matches']:
-        print (match)
-    return True,True
+    def insertSingleMatch(match):
+    '''Attempts to insert a match
+    Arg: dictionary of Match
+
+    Returns: a response message 
+
+    '''
+    print("attempting to insert match")
+    try:
+        dbConnection = ActiveDatabase().getConn()
+        matchConnection = dbConnection.collection(MATCHES)
+
+        matchConnection.document(match['matchId']).set(match)
+
+        return True
+
+    except Exception as e:
+        print(e)
+
+        return False
+    #TODO
+    #success
+    #accessed database but id was taken
+    #could not access database
 
 #TODO implement logger
 def main():
     
     print("Running MatchFinder")
 
-    #accessing active database
-    dbConnection = ActiveDatabase().getConn()
-    userConnection= dbConnection.collection(u'users')
-    try:
-        userList = userConnection.stream()
-    except Exception as e:
-        print(e)
-        userList = None
-
-    for user in userList:
-        user = user.to_dict()
-        user = User(user['region'],user['summoner_id'],user['account_id'])
-        matchList, matchListSuccess = fetchMatchEntries(user.toDict())
+    userList, userListSuccess = fetchActiveUsers()
+    if userListSuccess:
+        for user in userList:
+            print("Users successfully queried")
+            user = user.to_dict()
+            user = User(user['region'],user['summoner_id'],user['account_id'])
+            matchList, matchListSuccess = fetchMatchEntries(user.toDict())
+            if matchListSuccess:
+                print("Matches successfully queried")
+                for match in matchList:
+                    match = Match(match['gameId'],match['timestamp']).toDict()
+                    
+                    insertMatchSuccess = insertSingleMatch(match)
+                    if insertMatchSuccess:
+                        print("need to query for match details")
+                    else:
+                        print("Match failed skipping user")
+                        break
+            else:
+                print("Failed to retrieve any active runs")
+    else:
+        print("Failed to retrieve any active users")
 
     #steps 
     #Get active users
